a9709fd3a398d37d7b3f851dede37c7d939792fe
hshivaraj <harish.shivaraj@parkpaywireless.com>
2016-08-02 22:34:44 +0100

    Refectored the Coordinate class

--- cpp/gps.cpp
+++ cpp/gps.cpp
@@ -5,71 +5,105 @@ using namespace std;
 
 namespace constants
 {
-    const double pi(3.14159);   
+    const double pi(3.14159);
+    const double km(6371);
 }
 
-
-class Point
-{
-    private:
-        double value;
-
-    public:
-        Point(double v=0.0) : value(v) { }
-
-        float get(void) const { return value; }
-        void  set(double v) { value = v; };
-};
 
 class Coordinate
 {
     private:
-        Point latitude;
-        Point longitude;
+        double lat;
+        double log;
 
     public:
-        Coordinate(double lat=0.0, double log=0.0) : latitude(lat), longitude(log) { }
+        Coordinate(double lat=0.0, double log=0.0) : lat(lat), log(log) { }
 
-        float get_latitude(void)  const    { return latitude.get();  }
-        float get_longitude(void) const    { return longitude.get(); }
-        void  set_latitude(double v)             { latitude.set(v);  }
-        void  set_longitude(double v)            { longitude.set(v); }
-        
+        float latitude(void)  const    { return lat;  }
+        float longitude(void) const    { return log; }
+        void  latitude(double v)       { lat = v;  }
+        void  longitude(double v)      { log = v; }
+
         friend std::ostream& operator<< (std::ostream &out, const Coordinate &c)
         {
-            out<< "(" << c.get_latitude() << ", " << c.get_longitude() << ")";;
-            return out;
+            out<< "(" << c.latitude() << ", " << c.longitude() << ")";;
+             return out;
         }
 };
 
-double toRadian(double v) 
+
+double toRadian(double v)
 {
     return (v * constants::pi) / 180;
 }
 
-double distance(Coordinate c1, Coordinate c2) 
+double distance(Coordinate c1, Coordinate c2)
 {
-    double dLat = toRadian(c2.get_latitude() - c1.get_latitude());
-    double dLon = toRadian(c2.get_longitude() - c1.get_longitude());
-    double lat1 = toRadian(c1.get_latitude());
-    double lat2 = toRadian(c2.get_latitude());
+    double dLat = toRadian(c2.latitude() - c1.latitude());
+    double dLon = toRadian(c2.longitude() - c1.longitude());
+    double lat1 = toRadian(c1.latitude());
+    double lat2 = toRadian(c2.latitude());
 
     double a = sin(dLat/2) * sin(dLat/2) +
                 sin(dLon/2) * sin(dLon/2) * cos(lat1) * cos(lat2);
     double c = 2 * atan2(sqrt(a), sqrt(1-a));
-    return 6371 * c;
+    return constants::km * c;
 }
 
+double distance(Coordinate (&ep)[2])
+{
+        return distance(ep[0], ep[1]);
+}
+
+class Circle
+{
+    private:
+        Coordinate cp;
+        double rd;
+
+    public:
+        Circle(Coordinate c, int r) : cp(c), rd(r) {}
+
+};
+
+class Line
+{
+    private:
+        Coordinate ep[2];
+        double d;
+
+    public:
+        Line(Coordinate ps[2]) : ep(ps) 
+        {
+            d = distance(ep);
+        }
+
+        Line(Coordinate p1=0.0, Coordinate p2=0.0) : ep[0](p1), ep[1](p2)
+        {
+            d = distance(ep);
+        }
+
+        double distance(void) {
+            return d;
+        }
+};
+
+
+/* Calculate if the line intersects the circle */
+
+double checkIntersection(Line l, Circle c)
+{
+    return 0.0;
+}
 
 int main(void)
 {
-    Coordinate c1(52.225606, 0.087515);
-    Coordinate c2(52.214042, 0.110569);
-    cout << c1 << endl;
-    cout << c2 << endl;
+    Coordinate c[2];
+    c[0] = new Coordinate(52.225649, 0.087285);
+    c[1] = new Coordinate(52.214051, 0.110539);
 
-    cout << distance(c1,c2) << endl;
-
+    Circle c(new Coordinate(2.225606, 0.087515));
+    Line l(c);
 
     //cout << toRadians(c1.get_latitude()) << endl;
     
